625dd5a8ad56c12afba0b5193b5e95093266a7e6
Cody <burningwolf96@gmail.com>
2016-02-13 20:26:22 -0600

    Add ability to have script in subfolder

--- Tools/generate_timeline.ps1
+++ Tools/generate_timeline.ps1
@@ -1,12 +1,17 @@
-$excel = new-object -comobject excel.application
+$location = Get-Location
+# Gets the path of the parent directory
+$parent   = (get-item $location ).parent.FullName
+$file     = "$parent\ExcelDino.xlsm"
+$excel    = new-object -comobject excel.application
 
-$workbook = $excel.workbooks.open("Dinosaur.xlsm")
+$workbook = $excel.workbooks.open($file)
 $worksheet = $workbook.worksheets.item(1)
-$excel.Run("SortAndExport")
+# Runs the macro that sorts and exports the excel file to .csv
+$excel.Run("DinoSortExport")
 $workbook.save()
 $workbook.close()
 
-python "git version.py"
+python "../tiki-toki.py"
 
 $excel.quit
 pause
